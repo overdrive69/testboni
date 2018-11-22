19d7d410b44ec43acb5975dccc977692a883a4ac
overdrive69 <michael.rindhauser@gmx.at>
2018-11-22 17:51:29 +0100

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,7 +1,7 @@
 const burger_product = document.getElementById('menuswitchprod');
 const burger_other = document.getElementById('menuswitchable');
 
-if if( typeof burger_product !== 'undefined' ) {
+if ( typeof burger_product !== 'undefined' ) {
     $(burger_other).click();
 } else {
     $(burger_product).click()
